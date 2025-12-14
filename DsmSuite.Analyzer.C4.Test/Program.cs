fc5ffb1b35446147bb2857c6e95a645a5831642d
ernstaii <hn.deridder@avans.nl>
2025-12-14 12:10:25 +0000

    Added jgquiroga/dsmviz.analyzer.c4@f93eaa7
    Literal copy, doesn't compile yet!

---
+++ DsmSuite.Analyzer.C4.Test/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DsmSuite.Analyzer.C4.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
